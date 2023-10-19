d3c8ea4a412571abc54cd757a0eb4eedb37fc915
Tayyeba <135642166+TayyebaSadaq@users.noreply.github.com>
2023-10-19 20:42:32 +0100

    comi

---
+++ TextBasedAdventureGame/MainCode.py
@@ -0,0 +1,6 @@
+def Welcome():
+    print("""Welcome to your adventure! Oh mighty traveller.
+          You're currently embarking on a journey to the unknown.Trepsing through the forest, 
+          you come across a fork in the road. Do you go left or right?""")
+    
+Welcome()
