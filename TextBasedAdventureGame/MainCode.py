d25cab9af854337f45d1652a21cf2239169858ae
Tayyeba <135642166+TayyebaSadaq@users.noreply.github.com>
2023-10-29 14:51:11 +0000

    Delete TextBasedAdventureGame directory

--- TextBasedAdventureGame/MainCode.py
+++
@@ -1,6 +0,0 @@
-def Welcome():
-    print("""Welcome to your adventure! Oh mighty traveller.
-          You're currently embarking on a journey to the unknown.Trepsing through the forest, 
-          you come across a fork in the road. Do you go left or right?""")
-    
-Welcome()
